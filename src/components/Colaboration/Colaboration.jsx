eb33efffd6091b78c733780141de0a811ede267e
David Romero <dvdrmrrdz@gmail.com>
2023-10-04 02:17:59 -0600

    estilos para desktop

--- src/components/Colaboration/Colaboration.jsx
+++ src/components/Colaboration/Colaboration.jsx
@@ -8,10 +8,12 @@ export default function Colaboration() {
       <div className="project--msi">
         <h2>20th ANNIVERSARY PROJECT</h2>
         <p>New Anniversary Events and Items</p>
-        <picture>
-          <source className='project--imgPc' srcSet={msi} media='(width >= 768px)'/>
-          <img src={msiMobile} alt="" />
-        </picture>
+        <a href="#">
+          <picture>
+            <source className='project--imgPc' srcSet={msi} media='(width >= 768px)'/>
+            <img src={msiMobile} alt="" />
+          </picture>
+        </a>
         <p>Plus, look out for other exciting campaigns planned for the 20th anniversary!</p>
       </div>
     </section>
